7dfb5ba84911fe3ba4100c7b72a46fcc6f0ca707
RNogales <rnogales@correo.ugr.es>
2017-10-24 00:29:05 +0200

    Fin Practica 2

--- P1/app.py
+++ P1/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from flask import session, redirect, url_for, escape, request
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,8 +15,15 @@ def gatitos():
 def page_not_found(e):
     return render_template('404.html'), 404
 
-@app.route('/users/<string>:username')
+@app.route('/users/<username>')
 def profile(username):
-    return render_template('profile.html')
+    return render_template('profile.html', username=username)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+   if request.method == 'POST':
+      session['username'] = request.form['username']
+      return redirect(url_for('profile'), username)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
